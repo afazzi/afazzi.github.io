e947273e144136a0c258264c4f4bc59a7a5d1f3b
afazzi <afazzi@gmail.com>
2025-01-15 21:00:22 -0800

    updates

--- script.js
+++ script.js
@@ -1,24 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var categories = document.querySelectorAll('.category');
-
-    categories.forEach(function(category) {
-        category.addEventListener('click', function() {
-            var nextElement = category.nextElementSibling;
-            var categoryLink = category.querySelector('a');
-            while (nextElement && nextElement.tagName !== 'DIV') {
-                nextElement.classList.toggle('hidden');
-                nextElement = nextElement.nextElementSibling;
-            }
-            if (categoryLink) {
-                if (categoryLink.innerHTML.includes('-')) {
-                    categoryLink.innerHTML = categoryLink.innerHTML.replace('-', '+');
-                } else {
-                    categoryLink.innerHTML = categoryLink.innerHTML.replace('+', '-');
-                }
-            }
-        });
-    });
-
     // Detect iOS and add a class to the body
     if (/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream) {
         document.body.classList.add('ios');
