b09da4ee2ba82d1406fcab0d0d3ff17e64ba005c
Fazzi <afazzi@gmail.com>
2025-08-11 02:36:32 -0700

    updated posts

--- old/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Detect iOS and add a class to the body
-    if (/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream) {
-        document.body.classList.add('ios');
-    }
-});
